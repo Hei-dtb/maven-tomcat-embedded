e38ec40ea8909f827276b5dd1d4ad4e2ab18c773
Hei <hicham.bouzidi@gmail.com>
2014-09-22 21:40:22 +0200

    Create branch

---
+++ src/test/java/TomcatCommandLineTest.java
@@ -0,0 +1,36 @@
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+
+public class TomcatCommandLineTest {
+
+    @Test
+    public void test_default() throws ParseException {
+        TomcatCommandLine tcl = new TomcatCommandLine(new String[]{""});
+        tcl.parse();
+
+        assertThat(tcl.getPort()).isEqualTo(8080);
+        assertThat(tcl.isDaemon()).isFalse();
+    }
+
+    @Test
+    public void test_with_argument() throws ParseException {
+        TomcatCommandLine tcl = new TomcatCommandLine(new String[]{"-p", "9999", "-d"});
+        tcl.parse();
+        assertThat(tcl.getPort()).isEqualTo(9999);
+        assertThat(tcl.isDaemon()).isTrue();
+    }
+
+    @Test
+    public void test_fail_whith_bad_argument() {
+        TomcatCommandLine tcl = new TomcatCommandLine(new String[]{"-n", "noob"});
+        try {
+            tcl.parse();
+            failBecauseExceptionWasNotThrown(ParseException.class);
+        } catch (ParseException e) {
+            assertThat(e).hasMessage("Unrecognized option: -n");
+        }
+    }
+}
---
+++ src/test/java/TomcatEmbeddedIT.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+
+public class TomcatEmbeddedIT {
+
+    @Test
+    public void test_tomcat_launched() throws Exception {
+        URL url = new URL("http://localhost:8090");
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            assertThat(connection.getResponseMessage()).isEqualTo("OK");
+        } catch (Exception e) {
+            assertFalse("Tomcat must be launched to succeed", true);
+        }
+    }
+}
